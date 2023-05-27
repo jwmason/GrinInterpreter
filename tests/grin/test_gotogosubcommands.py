09ebd59585e40a2ba10835bce55a786ea7b5ff83
Mason Wong <masonjw1@uci.edu>
2023-05-27 00:40:35 -0700

    GOTO GOSUB Unittesting
    Made more unittests for gotogosubcommands.py and got up to 78% code coverage.

--- tests/grin/test_gotogosubcommands.py
+++ tests/grin/test_gotogosubcommands.py
@@ -4,7 +4,8 @@ import unittest
 import grin
 import contextlib
 import io
-from grin.gotogosubcommands import find_target_line
+from grin.gotogosubcommands import find_target_line, target_conditional
+from grin.inputconversion import generator_to_token
 
 
 class TestGotoGosubCommands(unittest.TestCase):
@@ -36,3 +37,51 @@ class TestGotoGosubCommands(unittest.TestCase):
         test_line_dict = {'A': 1}
         with self.assertRaises(Exception):
             find_target_line(test_target, test_line, test_line_dict)
+
+    def test_target_conditional_no_if(self):
+        line = generator_to_token(['GOTO 10'])
+        variable_dict = {}
+        line_dict = {'10': 2, '20': 3, '30': 4}
+        list_len = 10
+        current_line = 1
+        expected_result = None
+
+        actual_result = target_conditional(line, variable_dict, line_dict, list_len, current_line)
+
+        self.assertEqual(actual_result, expected_result)
+
+    def test_target_conditional_if_true(self):
+        line = generator_to_token(['GOTO 10 IF A > B'])
+        variable_dict = {'A': 5, 'B': 3}
+        line_dict = {'10': 2, '20': 3, '30': 4}
+        list_len = 10
+        current_line = 1
+        expected_result = None
+
+        actual_result = target_conditional(line, variable_dict, line_dict, list_len, current_line)
+
+        self.assertEqual(actual_result, expected_result)
+
+    def test_target_conditional_if_false(self):
+        line = generator_to_token(['GOTO 10 IF A > B'])
+        variable_dict = {'A': 3, 'B': 5}
+        line_dict = {'10': 2, '20': 3, '30': 4}
+        list_len = 10
+        current_line = 1
+        expected_result = None
+
+        actual_result = target_conditional(line, variable_dict, line_dict, list_len, current_line)
+
+        self.assertEqual(actual_result, expected_result)
+
+    def test_target_conditional_invalid_target(self):
+        line = generator_to_token(['GOTO 50'])
+        variable_dict = {}
+        line_dict = {'10': 2, '20': 3, '30': 4}
+        list_len = 10
+        current_line = 1
+        expected_result = None
+
+        actual_result = target_conditional(line, variable_dict, line_dict, list_len, current_line)
+
+        self.assertEqual(actual_result, expected_result)
